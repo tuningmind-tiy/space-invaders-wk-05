0945e75c1cdf91e083af9d78ce200aae71228abb
tuningmind <tuningmind@gmail.com>
2017-03-28 15:00:19 -0600

    Working demo rect

--- public/bundle.js
+++ public/bundle.js
@@ -76,6 +76,7 @@
     var screen = canvas.getContext('2d')
     var gameSize = { x: canvas.width, y: canvas.height }
     
+    this.bodies = [new Player(this, gameSize)]
     var self = this
     var tick = function() {
       self.update()
@@ -89,12 +90,31 @@
  
   Game.prototype = {
     update: function() {
-      console.log("hi")
               
     },
     draw: function(screen, gameSize) {
-      screen.fillRect(30, 30, 40, 40)
+      for (var i = 0; i < this.bodies.length; i++) {
+        drawRect(screen, this.bodies[i])
+      }
     }
+  }
+ 
+  var Player = function(game, gameSize) {
+    this.game = game
+    this.size = { x: 15, y: 15 }
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x }
+  }
+ 
+  Player.prototype = {
+    update: function() {
+
+    }
+  }
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y)
   }
   window.onload = function() {
     new Game("screen")
--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,7 @@
     var screen = canvas.getContext('2d')
     var gameSize = { x: canvas.width, y: canvas.height }
     
+    this.bodies = [new Player(this, gameSize)]
     var self = this
     var tick = function() {
       self.update()
@@ -20,8 +21,28 @@
               
     },
     draw: function(screen, gameSize) {
-      screen.fillRect(30, 30, 40, 40)
+      for (var i = 0; i < this.bodies.length; i++) {
+        drawRect(screen, this.bodies[i])
+      }
     }
+  }
+ 
+  var Player = function(game, gameSize) {
+    this.game = game
+    this.size = { x: 15, y: 15 }
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x }
+  }
+ 
+  Player.prototype = {
+    update: function() {
+
+    }
+  }
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y)
   }
   window.onload = function() {
     new Game("screen")
