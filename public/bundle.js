097597671b9a03951859612191c1522e419726cf
tuningmind <tuningmind@gmail.com>
2017-03-28 22:18:36 -0600

    Remove webpack for now

--- public/bundle.js
+++ public/bundle.js
@@ -70,7 +70,155 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-throw new Error("Module parse failed: /Users/tuningmind/dev/ironyard/space-invaders-wk-05/scripts/game.js Unexpected token (55:5)\nYou may need an appropriate loader to handle this file type.\n|         this.game.addBody(bullet);\n|       }\n|     };\n| \n|   var Invader = function(game, center) {");
+;(function () {
+  var Game = function(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var screen = canvas.getContext('2d');
+    var gameSize = { 
+      x: canvas.width, 
+      y: canvas.height 
+    };
+    this.bodies = createInvaders(this).concat(new Player(this, gameSize));
+    var self = this;
+    var tick = function() {
+      self.update();
+      self.draw(screen, gameSize);
+      requestAnimationFrame(tick);
+  };
+
+  tick();
+}
+ 
+  Game.prototype = {
+    update: function() {
+      for (var i = 0; i < this.bodies.length; i++) {
+        this.bodies[i].update();
+      }
+    },
+    draw: function(screen, gameSize) {
+      screen.clearRect(0, 0, gameSize.x, gameSize.y);
+      for (var i = 0; i < this.bodies.length; i++) {
+        drawRect(screen, this.bodies[i]);
+      }
+    },
+    addBody: function(body) {
+      this.bodies.push(body);
+    }
+  };
+ 
+  var Player = function(game, gameSize) {
+    this.game = game;
+    this.size = { x: 15, y: 15 };
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+    this.keyboarder = new Keyboarder();
+  };
+ 
+  Player.prototype = {
+    update: function() {
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+      }
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+        var bullet = new Bullet (
+            { x: this.center.x, y: this.center.y - this.size.x / 2 },
+            { x: 0, y: -6 });
+        this.game.addBody(bullet);
+      }
+    }
+  }
+
+  var Invader = function(game, center) {
+    this.game = game;
+    this.size = { x: 15, y: 15 };
+    this.center = center;
+    this.patrolX = 0;
+    this.speedX = 0.3;
+  };
+ 
+  Invader.prototype = {
+    update: function() {
+      if (this.patrolX < 0 || this.patrolX > 40) {
+        this.speedX = -this.speedX;
+      } 
+      this.center.x += this.speedX;
+      this.patrolX += this.speedX;
+    }
+  };
+
+
+  var Bullet = function(center, velocity) {
+    this.size = { x: 3, y: 3 };
+    this.center = center;
+    this.velocity = velocity;
+  };
+ 
+  Bullet.prototype = {
+    update: function() {
+        this.center.x += this.velocity.x;
+        this.center.y += this.velocity.y;
+    }
+  };
+
+
+  var Keyboarder = function() {
+    var keyState = {};
+
+    window.onkeydown = function(e) {
+      keyState[e.keyCode] = true;
+    };
+    window.onkeyup = function(e) {
+      keyState[e.keyCode] = false; 
+    };
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
+  }
+
+  var createInvaders = function(game) {
+    var invaders = [];
+    for (var i= 0; i < 24; i++) {
+      var x = 30 + (i % 8) * 30;
+      var y = 30 + (i % 3) * 30;
+      invaders.push(new Invader( game, { x:x, y: y}));
+    }
+    return invaders;
+  }
+
+  var loadSound = function(url, callback) {
+    var loaded = function() {
+      callback(sound);
+      sound.removeEventListener('canplaythrough', loaded);
+    };
+    
+    var sound = new Audio(url);
+    sound.addEventListener('canplaythrough', loaded);
+    sound.load();
+  };
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
+  };
+
+  var colliding = function(b1, b2) {
+    return !(b1 === b2 ||
+             b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+             b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+             b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+             b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+  )}
+
+  window.onload = function() {
+    new Game("screen");
+  };
+})()
+
+
 
 /***/ })
 /******/ ]);
--- scripts/game.js
+++ scripts/game.js
@@ -2,9 +2,11 @@
   var Game = function(canvasId) {
     var canvas = document.getElementById(canvasId);
     var screen = canvas.getContext('2d');
-    var gameSize = { x: canvas.width, y: canvas.height }
-    
-    this.bodies = [new Player(this, gameSize)];
+    var gameSize = { 
+      x: canvas.width, 
+      y: canvas.height 
+    };
+    this.bodies = createInvaders(this).concat(new Player(this, gameSize));
     var self = this;
     var tick = function() {
       self.update();
@@ -52,7 +54,8 @@
             { x: 0, y: -6 });
         this.game.addBody(bullet);
       }
-    };
+    }
+  }
 
   var Invader = function(game, center) {
     this.game = game;
@@ -71,6 +74,15 @@
       this.patrolX += this.speedX;
     }
   };
+  var createInvaders = function(game) {
+    var invaders = [];
+    for (var i= 0; i < 24; i++) {
+      var x = 30 + (i % 8) * 30;
+      var y = 30 + (i % 3) * 30;
+      invaders.push(new Invader( game, { x:x, y: y}));
+    }
+    return invaders;
+  }
 
   var Bullet = function(center, velocity) {
     this.size = { x: 3, y: 3 };
@@ -83,12 +95,6 @@
         this.center.x += this.velocity.x;
         this.center.y += this.velocity.y;
     }
-  };
-
-  var drawRect = function(screen, body) {
-    screen.fillRect(body.center.x - body.size.x / 2,
-                    body.center.y - body.size.y / 2,
-                    body.size.x, body.size.y);
   };
 
   var Keyboarder = function() {
@@ -106,6 +112,33 @@
 
     this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
   }
+
+
+  var loadSound = function(url, callback) {
+    var loaded = function() {
+      callback(sound);
+      sound.removeEventListener('canplaythrough', loaded);
+    };
+    
+    var sound = new Audio(url);
+    sound.addEventListener('canplaythrough', loaded);
+    sound.load();
+  };
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
+  };
+
+  var colliding = function(b1, b2) {
+    return !(b1 === b2 ||
+             b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+             b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+             b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+             b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+  )}
+
   window.onload = function() {
     new Game("screen");
   };
